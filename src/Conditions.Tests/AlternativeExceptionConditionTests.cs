381125a70ed958b87881621f6c53dc9d9ae42cd0
romkatv <roman.perepelitsa@gmail.com>
2015-12-14 12:05:36 +0100

    Enable WithExceptionOnFailure that has been previously commented out.

--- src/Conditions.Tests/AlternativeExceptionConditionTests.cs
+++ src/Conditions.Tests/AlternativeExceptionConditionTests.cs
@@ -8,44 +8,41 @@ namespace Conditions.Tests
     public class AlternativeExceptionConditionTests
     {
         [TestMethod]
-        [Ignore]
         [Description(
             "WithExceptionOnFailure called multiple times with the same exception type, always returns the same instance."
             )]
         public void WithExceptionOnFailure_CalledMultipleTimesWithTheSameExceptionType_ReturnsTheSameInstance()
         {
             // Act
-            //var condition1 = Condition.WithExceptionOnFailure<InvalidOperationException>();
-            //var condition2 = Condition.WithExceptionOnFailure<InvalidOperationException>();
+            var condition1 = Condition.WithExceptionOnFailure<InvalidOperationException>();
+            var condition2 = Condition.WithExceptionOnFailure<InvalidOperationException>();
 
             // Assert
-            //string assertMessage = "Two calls to WithExceptionOnFailure for the same exception type are " +
-            //                       "expected to return the same instance for performance reasons.";
+            string assertMessage = "Two calls to WithExceptionOnFailure for the same exception type are " +
+                                   "expected to return the same instance for performance reasons.";
 
-            //Assert.AreEqual(condition1, condition2,
-            //    assertMessage);
+            Assert.AreEqual(condition1, condition2,
+                assertMessage);
         }
 
         [TestMethod]
-        [Ignore]
         [Description(
             "WithExceptionOnFailure called multiple times with the different exception type, always returns different instances."
             )]
         public void WithExceptionOnFailure_CalledWithDifferentExceptionType_ReturnsDifferentInstances()
         {
             // Act
-            //var condition1 = Condition.WithExceptionOnFailure<InvalidOperationException>();
-            //var condition2 = Condition.WithExceptionOnFailure<ObjectDisposedException>();
+            var condition1 = Condition.WithExceptionOnFailure<InvalidOperationException>();
+            var condition2 = Condition.WithExceptionOnFailure<ObjectDisposedException>();
 
             // Assert
-            //string assertMessage = "Two calls to WithExceptionOnFailure for different exception type are " +
-            //                       "expected to return different instances, because each type will get its own condition type.";
+            string assertMessage = "Two calls to WithExceptionOnFailure for different exception type are " +
+                                   "expected to return different instances, because each type will get its own condition type.";
 
-            //Assert.AreNotEqual(condition1, condition2, assertMessage);
+            Assert.AreNotEqual(condition1, condition2, assertMessage);
         }
 
         [TestMethod]
-        [Ignore]
         [ExpectedException(typeof(ArgumentException))]
         [Description(
             "WithExceptionOnFailure called with an abstract exception type throws an exception of type ArgumentException."
@@ -53,11 +50,10 @@ namespace Conditions.Tests
         public void WithExceptionOnFailure_WithAbstractExceptionType_ThrowsExpectedExceptionType()
         {
             // Act
-            //Condition.WithExceptionOnFailure<AbstractException>();
+            Condition.WithExceptionOnFailure<AbstractException>();
         }
 
         [TestMethod]
-        [Ignore]
         [Description(
             "WithExceptionOnFailure called with an invalid exception throws an exception with the expected message.")]
         public void WithExceptionOnFailure_WithInvalidExceptionType_ThrowsExceptionWithExpectedMessage()
@@ -69,7 +65,7 @@ namespace Conditions.Tests
             try
             {
                 // Act
-                //Condition.WithExceptionOnFailure<NoValidConstructorException>();
+                Condition.WithExceptionOnFailure<NoValidConstructorException>();
 
                 // Assert
                 Assert.Fail("Exception was expected.");
@@ -81,7 +77,6 @@ namespace Conditions.Tests
         }
 
         [TestMethod]
-        [Ignore]
         [Description(
             "WithExceptionOnFailure called with an invalid exception throws an exception with the expected parameter name."
             )]
@@ -93,7 +88,7 @@ namespace Conditions.Tests
             try
             {
                 // Act
-                //Condition.WithExceptionOnFailure<NoValidConstructorException>();
+                Condition.WithExceptionOnFailure<NoValidConstructorException>();
 
                 // Assert
                 Assert.Fail("Exception was expected.");
@@ -105,19 +100,17 @@ namespace Conditions.Tests
         }
 
         [TestMethod]
-        [Ignore]
         [Description("WithExceptionOnFailure called with a valid exception type does not return null.")]
         public void WithExceptionOnFailure_WithValidExceptionType_ReturnsAnInstance()
         {
             // Act
-            //var condition = Condition.WithExceptionOnFailure<InvalidOperationException>();
+            var condition = Condition.WithExceptionOnFailure<InvalidOperationException>();
 
             // Assert
-            //Assert.IsNotNull(condition);
+            Assert.IsNotNull(condition);
         }
 
         [TestMethod]
-        [Ignore]
         [ExpectedException(typeof (ObjectDisposedException))]
         [Description(
             "WithExceptionOnFailure called with a valid exception throws that same type in case of a validation failure."
@@ -125,14 +118,13 @@ namespace Conditions.Tests
         public void WithExceptionOnFailure_WithValidExceptionType1_ThrowsSpecifiedExceptionOnValidationFailure()
         {
             // Arrange
-            //string value = null;
+            string value = null;
 
             // Act
-            // Condition.WithExceptionOnFailure<ObjectDisposedException>().Requires(value).IsNotNull();
+            Condition.WithExceptionOnFailure<ObjectDisposedException>().Requires(value).IsNotNull();
         }
 
         [TestMethod]
-        [Ignore]
         [ExpectedException(typeof (ApplicationException))]
         [Description(
             "WithExceptionOnFailure called with a valid exception throws that same type in case of a validation failure."
@@ -140,14 +132,15 @@ namespace Conditions.Tests
         public void WithExceptionOnFailure_WithValidExceptionType2_ThrowsSpecifiedExceptionOnValidationFailure()
         {
             // Arrange
-            //string value = null;
+            string value = null;
 
             // First call WithExceptionOnFailure for another exception type.
-            // Condition.WithExceptionOnFailure<KeyNotFoundException>();
+            Condition.WithExceptionOnFailure<KeyNotFoundException>();
 
             // Act 
-            // Condition.WithExceptionOnFailure<ApplicationException>().Requires(value).IsNotNull();
+            Condition.WithExceptionOnFailure<ApplicationException>().Requires(value).IsNotNull();
         }
+
         public abstract class AbstractException : Exception
         {
             public AbstractException(string message)
@@ -157,7 +150,34 @@ namespace Conditions.Tests
 
         public class NoValidConstructorException : Exception
         {
+            // Not enough parameters.
             public NoValidConstructorException()
+            {
+            }
+
+            // Wrong parameter type.
+            public NoValidConstructorException(int n)
+            {
+            }
+
+            // Too many parameters.
+            public NoValidConstructorException(string s, string q = null)
+            {
+            }
+
+            // Output parameter.
+            public NoValidConstructorException(out string s)
+            {
+                s = null;
+            }
+
+            // Reference parameter.
+            public NoValidConstructorException(ref string s, string q = null)
+            {
+            }
+
+            // Private.
+            private NoValidConstructorException(string s)
             {
             }
         }
