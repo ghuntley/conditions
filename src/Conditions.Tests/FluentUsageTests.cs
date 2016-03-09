1b1f995e8286753788ce7abdbbfa8b5be131100c
Paweł Iżycki <pawel.izycki@connectis.com>
2016-03-09 15:33:56 +0100

    Add 'this' modifier for `Required` and `Ensures` methods. From now on those methods can be accessed directly calling on object that is under the test. This makes code more readable.

---
+++ src/Conditions.Tests/FluentUsageTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Conditions.Tests
+{
+    [TestClass]
+    public class FluentUsageTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        [Description("Calling IsNotNull on null should fail.")]
+        public void IsNotNullFluently()
+        {
+            object o = null;
+            o.Requires().IsNotNull();
+        }
+    }
+}
